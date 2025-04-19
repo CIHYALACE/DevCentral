1e9f2bc8195e98353be7e14d2ed89ca85d93056a
Nadia20610 <nadia.ahmed.mohamed20.com>
2025-04-19 23:23:46 +0200

    local changes before switching to master branch

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -4,6 +4,7 @@ import HealthFitness from "../components/HealthANDFitness"
 import RecommendedAppsSection from "../components/RecommendedApps"
 import SpecialOffer from "../components/SpecialOffer"
 import {BannerSection} from "../components/BannerSection"
+
 export default function HomePage() {
     return (
         <>
