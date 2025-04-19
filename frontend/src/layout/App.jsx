c3d7b833b67fc62bfbb662e4da0ee3baa37b471e
waheedSALAH <w55h44@gmail.com>
2025-04-20 00:24:01 +0200

    Add login and registration pages; implement hero slider and health & fitness sections

--- frontend/src/layout/App.jsx
+++ frontend/src/layout/App.jsx
@@ -1,7 +1,9 @@
 import '../style/App.css'
 import { Routes, Route } from 'react-router-dom'
 import SharedLayout from '../sharedLayout/SharedLayout'
-import HomePage from '../pages/HomePage'
+import HomePage from '../pages/homePage'
+import Login from '../pages/login'
+import Register from '../pages/register'
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Route path="/" element={<SharedLayout />}>
         <Route path="/" element={<HomePage />} />
       </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   )
 }
--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -1,9 +1,18 @@
 import GamesSection from "../components/gamesSection"
-
+import HeroSlider from "../components/HeroSlider"
+import HealthFitness from "../components/HealthANDFitness"
+import RecommendedAppsSection from "../components/RecommendedApps"
+import SpecialOffer from "../components/SpecialOffer"
 export default function HomePage() {
     return (
         <>
             <GamesSection/>
+
+            <HeroSlider/>
+            <RecommendedAppsSection/>
+            <HealthFitness/>
+            <SpecialOffer/>
+
         </>
     )
 }
