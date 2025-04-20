0a893015e3cd856d15ea75bcb9a9090a3526f609
AbdelRahman <abodyoussef2015@gmail.com>
2025-04-20 11:21:24 +0200

    First Migration

--- DevCentral/DevCentral/settings.py
+++ DevCentral/DevCentral/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -98,6 +100,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
+
 
 
 # Internationalization
