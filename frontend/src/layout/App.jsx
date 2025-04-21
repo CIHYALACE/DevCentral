af189781a8851ad09964ca8513413401bd065c01
AbdelRahman <abodyoussef2015@gmail.com>
2025-04-21 12:29:15 +0200

    Put Things Up After Merg

--- frontend/src/layout/App.jsx
+++ frontend/src/layout/App.jsx
@@ -8,7 +8,7 @@ import ItemDetails from '../pages/appDetails' // Assuming this is the correct pa
 import GamesPage from '../pages/games' // Assuming this is the correct path to your games component
 import AppsPage from '../pages/applications'
 
-function App() {
+export default function App() {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
@@ -16,11 +16,9 @@ function App() {
         <Route path="/games" element={<GamesPage />} />
         <Route path="/apps" element={<AppsPage />} /> {/* Assuming you want to show the same page for apps */}
         <Route path="/details/:type/:id" element={<ItemDetails />} />
-      </Route>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-
-      
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Route>      
     </Routes>
   )
 }
