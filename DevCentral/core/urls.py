8b123fdec0c09fbd706cc02f039c524af5b94eb9
AbdelRahman <abodyoussef2015@gmail.com>
2025-04-21 17:14:34 +0200

    get back the old commits

--- DevCentral/core/urls.py
+++ DevCentral/core/urls.py
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 from django.urls import path
 from .views import *
 
 urlpatterns = [
     path('', AppViewSet.as_view({'get': 'list'}), name='Apps'),
+    path('productivity/', ProductivityAppset.as_view({'get': 'list'}), name='ProductivityApps'),
 ]
-=======
->>>>>>> parent of 1e6eff0 (Merge branch 'master' of github.com:CIHYALACE/DevCentral)
--- DevCentral/core/views.py
+++ DevCentral/core/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-<<<<<<< HEAD
 from .models import *
 from rest_framework import viewsets
 from .serializers import *
@@ -9,8 +8,11 @@ class AppViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return App.objects.filter(is_published=True)
-    
-=======
->>>>>>> parent of 1e6eff0 (Merge branch 'master' of github.com:CIHYALACE/DevCentral)
+
+class ProductivityAppset(viewsets.ModelViewSet):
+    serializer_class = AppSerializer
+
+    def get_queryset(self):
+        return App.objects.filter(catogory__name='Productivity')
 
 # Create your views here.
