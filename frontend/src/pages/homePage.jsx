d16cfae3d9e50f310c66eb847d906e61a4360a02
Nadia20610 <nadia.ahmed.mohamed20.com>
2025-04-23 11:16:48 +0200

    WIP: local changes on homepage

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -13,6 +13,7 @@ export default function HomePage() {
             <GamesSection/>
             <BannerSection/>
 
+
              <HeroSlider/>
             <RecommendedAppsSection/>
             <HealthFitness/>
