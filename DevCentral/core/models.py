9c410ebd0c1ad53b9d9b01b93f802bdc1cf75a48
Nadia20610 <nadia.ahmed.mohamed20.com>
2025-04-25 14:50:05 +0300

    added tag model and delevoper profile model

--- DevCentral/core/models.py
+++ DevCentral/core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db.models import Avg
+from django.contrib.auth.models import User
 
 User = get_user_model()
 from django.core.validators import MinValueValidator, MaxValueValidator
@@ -112,6 +113,30 @@ class App(models.Model):
     Approximate_size = models.CharField(max_length=50, null=True, blank=True)
     install_link = models.URLField(null=True, blank=True)
     is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class DeveloperProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    website = models.URLField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Program(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    tags = models.ManyToManyField(Tag, related_name='programs', blank=True)
 
     def __str__(self):
         return self.name
