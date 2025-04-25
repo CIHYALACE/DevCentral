3580503fc74605bfd437079d5d0bd702ee1e2ea3
AbdelRahman <abodyoussef2015@gmail.com>
2025-04-25 12:24:54 +0300

    Move (Privacy And Policy, Terms&condition, UserProfile) Pages From Components folder tompages folder

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import Sidebar from '../components/Sidebar'
 import ProfileHeader from '../components/ProfileHeader'
 import InfoSection from '../components/InfoSection'
-import UserProfile from '../components/UserProfile'
 import  '../style/UserProfile.css'
 
-export default function UserProfilePage() {
+export default function UserProfile() {
   return (
     <div className="user-profile-page">
       <Sidebar active="Your info" />
