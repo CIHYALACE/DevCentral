b16adc27b5b9d7cd33867dced0c295ae91bf000d
amr <amr512hossam@gmail.com>
2025-04-30 01:32:53 +0300

    streamlined and cleaned up the urls

--- DevCentral/core/urls.py
+++ DevCentral/core/urls.py
@@ -3,15 +3,11 @@ from django.urls import path, include
 from .views import *
 
 router = DefaultRouter()
-router.register('games', ProgramViewSet)
+router.register('', ProgramViewSet, basename="programs")
 router.register('reviews', ReviewViewSet)
 router.register('media', MediaViewSet)
-router.register('top-chart-apps', TopChartAppViewSet, basename="top-charts")
-router.register('productivity', ProductivityAppViewSet, basename="productivity")
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', ProgramViewSet.as_view({'get': 'list'}), name='Apps'),
-    path('<str:slug>/', ProgramViewSet.as_view({'get': 'retrieve'}), name='AppDetails')
 ]
 
