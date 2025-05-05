a3e749f46d7f4553f0845eea529069dc7212dfcb
AbdelRahman <abodyoussef2015@gmail.com>
2025-05-05 09:55:32 +0300

    lol

--- DevCentral/DevCentral/settings.py
+++ DevCentral/DevCentral/settings.py
@@ -235,14 +235,16 @@ SIMPLE_JWT = {
 }
 
 # CORS Settings
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://localhost:5173',
-    'http://127.0.0.1:3000',
-    'http://127.0.0.1:5173',
-    'http://192.168.1.4:5173',
-    'http://127.0.0.1:64720'
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+#     'http://localhost:5173',
+#     'http://127.0.0.1:3000',
+#     'http://127.0.0.1:5173',
+#     'http://192.168.1.4:5173',
+#     'http://127.0.0.1:64720'
+# ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 # If you want to allow credentials (cookies, authorization headers)
 CORS_ALLOW_CREDENTIALS = True
